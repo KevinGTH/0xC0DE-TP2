b43ed8c474b109905e43682122e4a28807896227
KevinGTH <kevin.bejarano@mi.unc.edu.ar>
2025-04-07 03:08:08 -0300

    fix: country_validation

--- src/gini_utils.py
+++ src/gini_utils.py
@@ -9,10 +9,11 @@ def get_latest_gini(data, target_country):
         date_str = entry.get('date')
         value = entry.get('value')
 
-        # Filtrar entradas sin país, sin valor o con value==None
+        # En caso de existir, buscamos el pais seleccionado y obtenemos sus datos
         if (not country or country.lower() != target_country.lower() or value is None):
             continue
 
+        #Verificacion del tipo de dato obtenido
         try:
             year = int(date_str)
             gini = float(value)
@@ -34,10 +35,10 @@ def country_validation(data, target_country):
     list_countries=data[1]
     valid_countries_name= set()     #Creo una lista con solo los nombres de los paises de la API
     for entry in list_countries:
-        country_name=entry.get('country', {}).get('value')
+        country_name=entry.get('country', {}).get('value').lower()
         if country_name:    #Me aseguro que solo se agregen paises con nombres distintos de None
             valid_countries_name.add(country_name)
-    if target_country not in valid_countries_name:
+    if target_country.lower() not in valid_countries_name:
         return False
     else:
         return True
