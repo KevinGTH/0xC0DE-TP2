b8370c7eafc20f2d7f86fb826bd17e4343cbe7c0
KevinGTH <kevin.bejarano@mi.unc.edu.ar>
2025-04-10 11:11:57 -0300

    add main.py for cleaner program structure

---
+++ without-assembler/main.py
@@ -0,0 +1,8 @@
+from server_interface import GiniAPIClient,GUI
+from gini_calc import  GiniCalculator
+if __name__ == '__main__':
+    API_URL = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1"
+    api_client = GiniAPIClient(API_URL)
+    calculator = GiniCalculator()
+    ui = GUI(api_client, calculator)
+    ui.run()
